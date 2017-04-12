5096b2529709f83a39a5f0cd18658c4e6bd627e2
Little Von <122691411@qq.com>
2017-04-12 23:37:46 +0800

    更改网址，加入之前漏掉的header头

--- net.py
+++ net.py
@@ -7,39 +7,42 @@ import re
 import subprocess
 import json
 import requests
-res = requests.get(url='http://xyz.ishadow.online/')
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+}
+res = requests.get(url='http://free.ishadow.online/', headers=headers)
 
 if res.status_code:
-	res.encoding = 'utf-8'
-	
-	content = res.text
+    res.encoding = 'utf-8'
+
+    content = res.text
 
 #########################################
-	#爬取Japan第一个服务器的密码
+    # 爬取Japan第一个服务器的密码
 #########################################
-	pattern = re.compile(
-	    '<h4>Password:<span id="pwjpa">(.*)</span>  <span class="copybtn" data-clipboard-target="#pwjpa"><i class="fa fa-copy"></i></span></h4>', re.S)
-	item = re.findall(pattern, content)
+    pattern = re.compile(
+        '<h4>Password:<span id="pwjpa">(.*)</span>  <span class="copybtn" data-clipboard-target="#pwjpa"><i class="fa fa-copy"></i></span></h4>', re.S)
+    item = re.findall(pattern, content)
 
-	password = item[0]
-	# print(password)
-	
-	subprocess.call('taskkill /f /im shadowsocks.exe', stdout=subprocess.PIPE)
-	# 更换为你的ss配置文件路径
-	ssconfigpath = "E:\Shadowsocks\gui-config.json"
-	with open(ssconfigpath, "r+") as f:
-	    data = json.load(f)
-	    data[u"configs"][0][u"password"] = password
-	    f.seek(0)
-	    json.dump(data, f, indent=4)
+    password = item[0]
+    # print(password)
 
-	# 更改为你的ss程序路径
-	sspath = "E:\Shadowsocks\Shadowsocks.exe"
-	subprocess.Popen(sspath)
+    subprocess.call('taskkill /f /im shadowsocks.exe', stdout=subprocess.PIPE)
+    # 更换为你的ss配置文件路径
+    ssconfigpath = "E:\Shadowsocks\gui-config.json"
+    with open(ssconfigpath, "r+") as f:
+        data = json.load(f)
+        data[u"configs"][0][u"password"] = password
+        f.seek(0)
+        json.dump(data, f, indent=4)
 
-	print("ok!")
+    # 更改为你的ss程序路径
+    sspath = "E:\Shadowsocks\Shadowsocks.exe"
+    subprocess.Popen(sspath)
+
+    print("ok!")
 
 else:
-	print("error!")
-
-
+    print("error!")
