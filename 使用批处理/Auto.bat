b21d563d4dc5656a827b40453a40e837b5364aca
Little Von <122691411@qq.com>
2017-07-19 01:38:55 +0800

    使用批处理,需要点dos知识

---
+++ 使用批处理/Auto.bat
@@ -0,0 +1,9 @@
+@echo off
+rem ---------------------------
+rem           记得加入路径
+rem 例如:
+rem e:
+rem cd Python_learning\Programs\Auto_ishadowsocks
+rem ---------------------------
+
+E:\1Code\Repositories\Auto_ishadow\shadowsocks.py
