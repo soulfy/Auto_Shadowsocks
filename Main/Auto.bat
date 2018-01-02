c1e959c3a952fe100bc4e3b9a9a1a2ebf98faf57
soulfy <soulfy@163.com>
2018-01-02 14:17:40 +0800

    合并原作者分支 简化 兼容python2.7

--- Main/Auto.bat
+++ Main/Auto.bat
@@ -1,7 +1,9 @@
 @echo off
-cd allServer
-rem select the server
-echo ----------------------1. Japan-A-Server----------------------
+:cd /d %~dp0\allServer
+set base=%~dp0\allServer
+
+rem 选择选项
+echo ----------------------1. Japan的A服务器----------------------
 echo.
 echo ----------------------2. Japan-B-Server----------------------
 echo.
@@ -17,41 +19,7 @@ echo ----------------------7. Usa-A-Server----------------------
 echo.
 CHOICE /C 1234567 /M Please--Input--the--Server--Num
 
-if %errorlevel%==1 goto JapanA
-if %errorlevel%==2 goto JapanB
-if %errorlevel%==3 goto JapanC
-if %errorlevel%==4 goto SingaporeA
-if %errorlevel%==5 goto SingaporeB
-if %errorlevel%==6 goto SingaporeC
-if %errorlevel%==7 goto UsaA
-
-:JapanA
-Japan_A_shadowsocks.py
-goto end
-
-:JapanB
-Japan_B_shadowsocks.py
-goto end
-
-:JapanC
-Japan_C_shadowsocks.py
-goto end
-
-:SingaporeA
-Singapore_A_shadowsocks.py
-goto end
-
-:SingaporeB
-Singapore_B_shadowsocks.py
-goto end
-
-:SingaporeC
-Singapore_C_shadowsocks.py
-goto end
-
-:UsaA
-USA_A_shadowsocks.py
-goto end
+python %base%\api_shadowsocks.py %errorlevel%
 
 :end
 pause
--- Main/allServer/api_shadowsocks.py
+++ Main/allServer/api_shadowsocks.py
@@ -5,9 +5,8 @@ import re
 import subprocess
 import json
 import requests
-import tkinter.messagebox
-import webbrowser
-import time
+import sys
+
 
 class ShadowSocks(object):
     """docstring for ShadowSocks
@@ -20,8 +19,13 @@ class ShadowSocks(object):
         getHtml(self)                                 # 获取页面内容
         printItem(self, pattern=JapanA_pattern)       # 获取服务器内容
     """
+    # 更改为你的ss程序路径
+    ssPath = "E:\Shadowsocks\Auto_Shadowsocks\Shadowsocks\Shadowsocks.exe"
 
-    def __init__(self, ssPath, ssConfigPath):
+    # 更换为你的ss配置文件路径
+    ssConfigPath = "E:\Shadowsocks\Auto_Shadowsocks\Shadowsocks\gui-config.json"
+	
+    def __init__(self):
         '''
         文档注释
         
@@ -31,8 +35,6 @@ class ShadowSocks(object):
       
         '''
         super(ShadowSocks, self).__init__()
-        self.ssPath = ssPath
-        self.ssConfigPath = ssConfigPath
         self.getHtml()
 
     # 获取页面内容
@@ -148,42 +150,43 @@ headers = {
 
 # 匹配日本服务器的模式
 JapanA_pattern = re.compile(
-    r'<span id=".*?jpa">(.+?)</span>.+<span id="port.*?">(.+?)</span>.+<span id="pwjpa">(.+?)</span>.+<h4>Method.(.+?)</h4>', re.S)
+    r'<span id="ipjpa">(.+?)</span>.+<span id="portjpa">(.+?)</span>.+<span id="pwjpa">(.+?)</span>.+<h4>Method:(.+?)</h4>.+jpa(?:.*)?.png', re.S)
 JapanB_pattern = re.compile(
-    r'<span id=".*?jpb">(.+?)</span>.+<span id="port.*?">(.+?)</span>.+<span id="pwjpb">(.+?)</span>.+<h4>Method.(.+?)</h4>', re.S)
+    r'<span id="ipjpb">(.+?)</span>.+<span id="portjpb">(.+?)</span>.+<span id="pwjpb">(.+?)</span>.+<h4>Method:(.+?)</h4>.+jpb(?:.*)?.png', re.S)
 JapanC_pattern = re.compile(
-    r'<span id=".*?jpc">(.+?)</span>.+<span id="port.*?">(.+?)</span>.+<span id="pwjpc">(.+?)</span>.+<h4>Method.(.+?)</h4>', re.S)
+    r'<span id="ipjpc">(.+?)</span>.+<span id="portjpc">(.+?)</span>.+<span id="pwjpc">(.+?)</span>.+<h4>Method:(.+?)</h4>.+jpc(?:.*)?.png', re.S)
 
 # 匹配新加坡服务器的模式
 SingaporeA_pattern = re.compile(
-    r'<span id=".*?sga">(.+?)</span>.+<span id="port.*?">(.+?)</span>.+<span id="pwsga">(.+?)</span>.+<h4>Method.(.+?)</h4>', re.S)
+    r'<span id="ipsga">(.+?)</span>.+<span id="portsga">(.+?)</span>.+<span id="pwsga">(.+?)</span>.+<h4>Method:(.+?)</h4>.+sga(?:.*)?.png', re.S)
 SingaporeB_pattern = re.compile(
-    r'<span id=".*?sgb">(.+?)</span>.+<span id="port.*?">(.+?)</span>.+<span id="pwsgb">(.+?)</span>.+<h4>Method.(.+?)</h4>', re.S)
+    r'<span id="ipsgb">(.+?)</span>.+<span id="portsgb">(.+?)</span>.+<span id="pwsgb">(.+?)</span>.+<h4>Method:(.+?)</h4>.+sgb(?:.*)?.png', re.S)
 SingaporeC_pattern = re.compile(
-    r'<span id=".*?sgc">(.+?)</span>.+<span id="port.*?">(.+?)</span>.+<span id="pwsgc">(.+?)</span>.+<h4>Method.(.+?)</h4>', re.S)
+    r'<span id="ipsgc">(.+?)</span>.+<span id="portsgc">(.+?)</span>.+<span id="pwsgc">(.+?)</span>.+<h4>Method:(.+?)</h4>.+sgc(?:.*)?.png', re.S)
 
 # 匹配美国服务器的模式
 UsaA_pattern = re.compile(
-    r'<span id=".*?usa">(.+?)</span>.+<span id="port.*?">(.+?)</span>.+<span id="pwusa">(.+?)</span>.+<h4>Method.(.+?)</h4>', re.S)
+    r'<span id="ipusa">(.+?)</span>.+<span id="portusa">(.+?)</span>.+<span id="pwusa">(.+?)</span>.+<h4>Method:(.+?)</h4>.+usa(?:.*)?.png', re.S)
 UsaB_pattern = re.compile(
-    r'<span id=".*?usb">(.+?)</span>.+<span id="port.*?">(.+?)</span>.+<span id="pwusb">(.+?)</span>.+<h4>Method.(.+?)</h4>', re.S)
+    r'<span id="ipusb">(.+?)</span>.+<span id="portusb">(.+?)</span>.+<span id="pwusb">(.+?)</span>.+<h4>Method:(.+?)</h4>.+usb(?:.*)?.png', re.S)
 UsaC_pattern = re.compile(
-    r'<span id=".*?usc">(.+?)</span>.+<span id="port.*?">(.+?)</span>.+<span id="pwusc">(.+?)</span>.+<h4>Method.(.+?)</h4>', re.S)
+    r'<span id="ipusc">(.+?)</span>.+<span id="portusc">(.+?)</span>.+<span id="pwusc">(.+?)</span>.+<h4>Method:(.+?)</h4>.+usc(?:.*)?.png', re.S)
 
 if __name__ == '__main__':
-    # 更改为你的ss程序路径
-    ssPath = "E:\Shadowsocks\Shadowsocks.exe"
-
-    # 更换为你的ss配置文件路径
-    ssConfigPath = "E:\Shadowsocks\gui-config.json"
-
-    a = ShadowSocks(ssPath, ssConfigPath)
-    a.printItem(pattern=JapanA_pattern)
-    a.printItem(pattern=JapanB_pattern)
-    a.printItem(pattern=JapanC_pattern)
-    a.printItem(pattern=SingaporeA_pattern)
-    a.printItem(pattern=SingaporeB_pattern)
-    a.printItem(pattern=SingaporeC_pattern)
-    a.printItem(pattern=UsaA_pattern)
-    a.printItem(pattern=UsaB_pattern)
-    a.printItem(pattern=UsaC_pattern)
+	patterns = [None, JapanA_pattern, JapanB_pattern, JapanC_pattern, 
+				SingaporeA_pattern, SingaporeB_pattern, SingaporeC_pattern,
+				UsaA_pattern]
+	patterns_str = [None, "JapanA_pattern", "JapanB_pattern", "JapanC_pattern", 
+				"SingaporeA_pattern", "SingaporeB_pattern", "SingaporeC_pattern",
+				"UsaA_pattern"]
+	
+	# 默认UsaA_pattern
+	select = 7
+	if len(sys.argv) > 1:
+		select = int(sys.argv[1])
+		if select > 7 or select < 1:
+			select = 7
+		
+	print("Use Server: {}".format(patterns_str[select]))
+	a = ShadowSocks()
+	a.setShadowSocks(pattern=patterns[select])
